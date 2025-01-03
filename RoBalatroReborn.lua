1bbc9df3cb1d95325c2130f4b34432b7da6b1fe6
AlexZGreat <alexzgreat1093@gmail.com>
2025-01-03 12:37:09 -0500

    First Commit!!!!!

---
+++ RoBalatroReborn.lua
@@ -0,0 +1,26 @@
+--- STEAMODDED HEADER
+
+--- MOD_NAME: Ro-Balatro Reborn
+--- MOD_ID: robalatroreborn
+--- MOD_AUTHOR: [AlexZGreat]
+--- MOD_DESCRIPTION: Remake of Ro-Balatro
+--- PRIORITY: -200
+--- BADGE_COLOR: fd5f55
+--- PREFIX: roblr
+--- VERSION: 1.0
+--- LOADER_VERSION_GEQ: 1.0.0
+
+--Mod icon
+SMODS.Atlas{
+    key = "modicon",
+    path = "robalatro_modicon.png",
+    px = 34,
+    py = 34,
+}
+
+--Load files
+assert(SMODS.load_file("objects/sounds.lua"))()
+assert(SMODS.load_file("objects/gears.lua"))()
+assert(SMODS.load_file("objects/places.lua"))()
+assert(SMODS.load_file("objects/redcliff.lua"))()
+assert(SMODS.load_file("objects/boosters.lua"))()
